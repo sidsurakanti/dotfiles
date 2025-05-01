400e554a5a2fe0c39c05271d9f2e848e8887980e
sidsurakanti <sidsurakanti1@gmail.com>
2025-05-01 19:47:33 -0400

    impr: added brew install for macos and cp instead of symlinking

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-echo "simlinking dotfiles"
+echo "copying dotfiles"
 
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+cp -b -s ~/dotfiles/.zshrc ~/.zshrc
+cp -b ~/dotfiles/.gitconfig ~/.gitconfig
+
 # make sure /.config exists we can use it
 mkdir -p ~/.config
-ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
-mkdir -p ~/.config/fastfetch
-ln -s ~/dotfiles/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+cp -b ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 
-echo "done!"
+mkdir -p ~/.config/fastfetch
+cp -b ~/dotfiles/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
+
+echo "dotfiles copied!"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+		echo "installing starship and fastfetch via brew"
+		brew install starship fastfetch
+		echo "done installing packages."
+else
+	echo "skipping brew install" 
+fi
+
+echo "all finished"
+
