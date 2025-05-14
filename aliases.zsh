d836e5a16405288a3eff82ae0202465ebca58cbd
sidsurakanti <sidsurakanti1@gmail.com>
2025-05-14 02:45:33 -0400

    more aliases + back to simlink

---
+++ aliases.zsh
@@ -0,0 +1,21 @@
+alias reloads="omz reload"
+
+alias projects="cd $HOME/projects"
+alias dotfiles="cd $DOTFILES"
+
+alias watch="npm run dev"
+
+alias commit="git add . && git commit -m"
+alias pull="git pull"
+alias push="git push"
+alias gs="git status"
+alias gb="git branch"
+alias gc="git checkout"
+alias stash="git stash -u"
+
+alias cls='clear'
+alias tr='tree -L 1'
+alias ..='cd ..'
+alias vi='nvim .'
+alias loc='git ls-files | xargs wc -l'
+alias vsc='code .'
--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "copying dotfiles"
+echo "siminking dotfiles"
 
-cp -b -s ~/dotfiles/.zshrc ~/.zshrc
-cp -b ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/aliases.zsh ~/aliases.zsh
 
 # make sure /.config exists we can use it
 mkdir -p ~/.config
-cp -b ~/dotfiles/.config/starship.toml ~/.config/starship.toml
-
+ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 mkdir -p ~/.config/fastfetch
-cp -b ~/dotfiles/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
+ln -s ~/dotfiles/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 
-echo "dotfiles copied!"
+echo "dotfiles linked!"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 		echo "installing starship and fastfetch via brew"
