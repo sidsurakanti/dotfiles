d82438fa195afe27507e191d85ec2ffcf6da531b
sidsurakanti <sidsurakanti1@gmail.com>
2025-05-14 02:55:49 -0400

    impr: add force flag for simlinks

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 echo "siminking dotfiles"
 
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/aliases.zsh ~/aliases.zsh
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/aliases.zsh ~/aliases.zsh
 
 # make sure /.config exists we can use it
 mkdir -p ~/.config
-ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 mkdir -p ~/.config/fastfetch
-ln -s ~/dotfiles/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.config/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 echo "dotfiles linked!"
 
