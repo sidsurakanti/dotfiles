98ad3efdbcf9c0c0a730bacbbd322d5cb62a7eb6
sidsurakanti <sidsurakanti1@gmail.com>
2025-05-28 20:00:00 -0700

    impr: updated wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,24 @@ local config = wez.config_builder()
 
 config.font_size = 18
 config.color_scheme = 'Tokyo Night'
-config.initial_rows = 80
+config.initial_rows = 40
 config.initial_cols = 160
+
+config.automatically_reload_config = true
+
+config.use_fancy_tab_bar = false
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
+config.window_padding = { left = 0, right = 0, top = 40, bottom = 10 }
+config.window_decorations = "NONE"
+config.tab_bar_at_bottom = true
+
+config.keys = {
+  {
+    key = 'n',
+    mods = 'SHIFT',
+    action = wez.action.ToggleFullScreen,
+  },
+}
 
 return config
