08fcc933301575cc7117c90394b8b4b2b0bb9bf1
sidsurakanti <sidsurakanti1@gmail.com>
2025-06-24 01:44:31 -0700

    chore: remove old config

--- .config/old_nvim/lua/configs/lspconfig.lua
+++
@@ -1,6 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "html", "cssls", "pyright" }
-vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers 
--- .config/old_nvim/lua/options.lua
+++
@@ -1,9 +0,0 @@
-require "nvchad.options"
-
--- add yours here!
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
