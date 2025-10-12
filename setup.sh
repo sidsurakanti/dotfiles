13d4d46beef47a111e2d3b452da96e6fc5eff4be
sidsurakanti <sidsurakanti1@gmail.com>
2025-10-11 22:55:52 -0400

    impr: added docker install script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 pause() {
   read -n 1 -s -r -p "$1"
   echo
@@ -10,21 +12,21 @@ read -p "are you resuming after oh my zsh install? (y/n): " resumed
 if [ "$resumed" != "y" ]; then
   pause "press any key to start setup script..."
 
-  pause "updating apt..."
+  pause "updating apt... (enter to continue)"
   sudo apt update && sudo apt upgrade -y
   sudo apt autoremove -y
   clear
 
-  pause "installing zsh shell..."
+  pause "installing zsh shell... (enter to continue)"
   sudo apt install -y zsh
   clear
 
-  echo "setting default shell as zsh"
+  echo "setting default shell as zsh (enter to continue)"
   chsh -s "$(which zsh)"
-  echo "will restart shell at the end of script"
+  echo "will restart shell at the end of script (enter to continue)"
 
   # https://github.com/ohmyzsh/ohmyzsh
-  pause "downloading oh my zsh... (select no for default shell)"
+  pause "downloading oh my zsh... (enter to continue)"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   clear
 fi
@@ -36,6 +38,7 @@ read -p "would you like to add github ssh? (y/n): " answer
 if [ "$answer" = "y" ]; then
   read -p "please enter the email you use for your github account: " user_email
   ssh-keygen -t ed25519 -C "$user_email"
+  clear
   cat ~/.ssh/id_ed25519.pub
   pause "please add the following key to github ssh"
 elif [ "$answer" = "n" ]; then
@@ -46,27 +49,27 @@ fi
 
 clear
 
-pause "syncing dotfiles..."
+pause "syncing dotfiles... (enter to continue)"
 cd ~/dotfiles || { echo "dotfiles not found"; exit 1; }
 chmod +x ./install.zsh
 ./install.zsh
 
-pause "installing linuxbrew..."
+pause "installing linuxbrew... (enter to continue)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 sudo apt-get install -y build-essential
 clear
 
-echo "setting up brew env"
+echo "setting up brew env... (enter to continue)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # TODO: UPDATE THIS TO SUDO APT
-pause "installing useful packages.."
+pause "installing useful packages... (enter to continue)"
 brew install gcc cmake neovim starship fastfetch ripgrep unzip tree cowsay lolcat git-lfs
 clear
 
-pause "installing nvm..."
+pause "installing nvm... (enter to continue)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 clear
 
-echo "restarting shell..."
+echo "restarting shell... (enter to continue)"
 exec zsh
